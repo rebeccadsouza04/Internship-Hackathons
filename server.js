73657fd4d7bad0e77ae50cdbc37c3e353b0c7b68
Rebecca Aurelia Dsouza <71254956+rebeccadsouza04@users.noreply.github.com>
2022-05-03 10:38:33 +0530

    Prepare for deployment

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.use('/api/recommendations', require('./routes/api/recommendation'));
 app.use('/api/hackathons', require('./routes/api/hackathon'));
 
 //Deployment on Heroku (Yet to complete)
-/*// Serve static assets in production
+// Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
   app.use(express.static('client/build'));
@@ -52,8 +52,8 @@ if (process.env.NODE_ENV === 'production') {
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
-}*/
+} 
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
